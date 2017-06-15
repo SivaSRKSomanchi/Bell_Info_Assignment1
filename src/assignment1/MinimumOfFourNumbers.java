700dedc63ce236eecf6091d4984213dc7b8d0fb2
SivaSRKSOmanchi <sivaramsomanchi555@gmail.com>
2017-06-15 19:42:23 -0400

    first commit

---
+++ src/assignment1/MinimumOfFourNumbers.java
@@ -0,0 +1,26 @@
+package assignment1;
+
+import java.util.Scanner;
+
+public class MinimumOfFourNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		int[] arary = new int[4];
+		int minNum=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 4 Numbers:");
+		for(int i=0;i<4;i++){
+			arary[i] = sc.nextInt();			
+		}
+		minNum=arary[0];
+		for(int i=1;i<4;i++){
+			if(arary[i]<minNum){
+				minNum = arary[i];
+			}
+		}
+		System.out.println("Mininum of four numbers is: "+minNum);
+
+	}
+
+}
---
+++ src/assignment1/oddNumbers.java
@@ -0,0 +1,18 @@
+package assignment1;
+
+public class oddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Printing Odd Number less than 100");
+		for(int i=0;i<100;i++){
+			if(i%2!=0){
+				System.out.print(i+" ");
+			}
+			
+		}
+		
+
+	}
+
+}
