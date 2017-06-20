426d3d3f11f8d220d6b339bc299668ad6279bcd3
SivaSRKSOmanchi <sivaramsomanchi555@gmail.com>
2017-06-19 21:03:40 -0400

    Small changes in Armstrong

--- src/assignment1/ArmstrongNum.java
+++ src/assignment1/ArmstrongNum.java
@@ -6,18 +6,30 @@ public class ArmstrongNum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n,sum=0,m=0;
+		int givenNum;
+		double sum=0.0, digits = 0.0;
+		double rem=0.0;
 		System.out.println("Enter the number: ");
 		Scanner sc = new Scanner(System.in); 
-		n = sc.nextInt();
-		int k=n;
-		while(n!=0){
-			m=n%10;
-			sum = sum + m*m*m;
-			n =n/10;			
+		givenNum = sc.nextInt();
+		 
+		int replica=givenNum;
+		
+		while(replica!=0){
+			digits++;       // num of digits of given number
+			replica/=10;
+		}
+		
+		double digit = (Math.floor(Math.log10(givenNum))) + 1;
+		replica = givenNum;
+		
+		while(givenNum!=0){
+			rem=givenNum%10;
+			sum = sum + Math.pow(rem, digit);
+			givenNum =givenNum/10;			
 		}
 		System.out.println();
-		if(k==sum){
+		if(replica==sum){
 			System.out.println("Number is Armstrong");
 		}
 		else{
--- src/assignment1/PerfectNumber.java
+++ src/assignment1/PerfectNumber.java
@@ -6,22 +6,21 @@ public class PerfectNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n, sum=0,m=0;
+		int n, sum = 0, m = 0;
 		System.out.println("Enter the number: ");
-		Scanner sc = new Scanner(System.in); 
+		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		int k=n;
-		for(int i=1;i<n;i++){
-			if(n%i==0){
+		int k = n;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
 				sum = sum + i;
 			}
 		}
-		
-		if(k==sum){
+
+		if (k == sum) {
 			System.out.println("Number is PerfectNumber");
-		}
-		else{
-			System.out.println("Number is not a PerfectNumber");	
+		} else {
+			System.out.println("Number is not a PerfectNumber");
 		}
 		sc.close();
 	}
