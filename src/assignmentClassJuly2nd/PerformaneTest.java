80c829328ff9424373a5e01f14f1c878982e9e89
SivaSRKSOmanchi <sivaramsomanchi555@gmail.com>
2017-07-03 03:42:29 -0400

    Added Random Funtion. So Insertion, Searching and Deletion done by passing random number as index.

--- src/assignmentClassJuly2nd/PerformaneTest.java
+++ src/assignmentClassJuly2nd/PerformaneTest.java
@@ -1,5 +1,6 @@
 package assignmentClassJuly2nd;
 
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
@@ -9,61 +10,78 @@ public class PerformaneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num = 100000;
+		int num = 1000000;
 		List<String> arrList = new ArrayList<>();
 		List<String> linkedList = new LinkedList<>();
-		
-		System.out.println("Insetion of Elements: LinkedList has better performance than ArrayList.");
+		creatingSize(arrList, num);
+		creatingSize(linkedList, num);
+		System.out
+				.println("Insetion of Elements: LinkedList has better performance than ArrayList.");
 		insertion(arrList, num);
 		insertion(linkedList, num);
 		System.out
 				.println("\nSearch/Access time for ArrayList: O(1). \nAccess time for LinkedList: O(n)");
 		System.out
 				.println("In an array, you can access to any element by using array[index], while in a linked list you must navigate \nthrough all the list starting from first until you get the element you need.");
-		
+
 		search(arrList, num);
 		search(linkedList, num);
-		System.out.println("\nDeletion Time is less for linked List. It just traverse through the list and deletes the specific node there");
-		System.out.println("While in ArrayList, it has to create duplicate array and deletes the required \nelement there and then merge the rest of the part.");
+		System.out
+				.println("\nDeletion Time is less for linked List. It just traverse through the list and deletes the specific node there");
+		System.out
+				.println("While in ArrayList, it has to create duplicate array and deletes the required \nelement there and then merge the rest of the part.");
 		delete(arrList, num);
 		delete(linkedList, num);
 
 	}
 
+	static void creatingSize(List<String> l, int num) {
+		for (int i = 0; i < num; i++) {
+			l.add("" + i);
+		}
+	}
+
 	private static void insertion(List<String> l, int num) {
 		long initialTime;
 		long finalTime;
+		Random r = new Random();
+		int n = r.nextInt(10000);
 		initialTime = System.currentTimeMillis();
-		for (int i = 0; i < num; i++) {
-			l.add(i, " Somanchi ");
+		for (int i = 0; i < 10000; i++) {
+			l.add(n, " Somanchi ");
 		}
 		finalTime = System.currentTimeMillis();
 		System.out.println("Insertion Time of " + l.getClass() + " is "
-				+ (finalTime - initialTime));
+				+ ((finalTime - initialTime) )+ " MilliSeconds");
 	}
 
 	private static void search(List<String> l, int num) {
 		long initialTime;
 		long finalTime;
+		
+		Random r = new Random();
+		int n = r.nextInt(10000);
 		initialTime = System.currentTimeMillis();
-		for (int i = 0; i < num; i++) {
-			l.get(i);
+		for (int i = 0; i < 10000; i++) {
+			l.get(n);
 		}
 		finalTime = System.currentTimeMillis();
 		System.out.println("Search Time of " + l.getClass() + " is "
-				+ (finalTime - initialTime));
+				+ (finalTime - initialTime) + " MilliSeconds");
 	}
 
 	private static void delete(List<String> l, int num) {
 		long initialTime;
 		long finalTime;
+		Random r = new Random();
+		int n = r.nextInt(10000);
 		initialTime = System.currentTimeMillis();
-		for (int i = 0; i < 5000; i++) {
-			l.remove(i);
+		for (int i = 0; i < 10000; i++) {
+			l.remove(n);
 		}
 		finalTime = System.currentTimeMillis();
 		System.out.println("Deletion Time of " + l.getClass() + " is "
-				+ (finalTime - initialTime));
+				+ ((finalTime - initialTime))+" MilliSeconds" );
 	}
 
 }
--- src/assignmentClassJuly2nd/ReadMe.txt
+++ src/assignmentClassJuly2nd/ReadMe.txt
@@ -1,16 +1,16 @@
 Insetion of Elements: LinkedList has better performance than ArrayList.
-Insertion Time of class java.util.ArrayList is 8
-Insertion Time of class java.util.LinkedList is 4
+Insertion Time of class java.util.ArrayList is 2010 MilliSeconds
+Insertion Time of class java.util.LinkedList is 470 MilliSeconds
 
 Search/Access time for ArrayList: O(1). 
 Access time for LinkedList: O(n)
 In an array, you can access to any element by using array[index], while in a linked list you must navigate 
 through all the list starting from first until you get the element you need.
-Search Time of class java.util.ArrayList is 4
-Search Time of class java.util.LinkedList is 4600
+Search Time of class java.util.ArrayList is 1 MilliSeconds
+Search Time of class java.util.LinkedList is 181 MilliSeconds
 
 Deletion Time is less for linked List. It just traverse through the list and deletes the specific node there
 While in ArrayList, it has to create duplicate array and deletes the required 
 element there and then merge the rest of the part.
-Deletion Time of class java.util.ArrayList is 65
-Deletion Time of class java.util.LinkedList is 24
+Deletion Time of class java.util.ArrayList is 1902 MilliSeconds
+Deletion Time of class java.util.LinkedList is 161 MilliSeconds
